2ed15f5b1a441d4e8d7e16a7b2d215ae2ea1638d
almacode1703 <testmygitt.2023@gmail.com>
2026-02-06 18:54:37 +0530

    updates

---
+++ src/Maggie.java
@@ -0,0 +1,14 @@
+public class Maggie {
+    public static void main(String[] args) {
+        System.out.println("Steps to create Maggie..");
+        Maggie();
+    }
+    public static void Maggie(){
+        System.out.println("1. Boil Water");
+        System.out.println("2. Pour Maggie Noddles");
+        System.out.println("3. Pour Maggie Masala");
+        System.out.println("4. Stir");
+    }
+}
+
+
