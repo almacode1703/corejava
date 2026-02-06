cbb04b55055922847ffc9122fee075458ce76862
almacode1703 <testmygitt.2023@gmail.com>
2026-02-07 01:00:46 +0530

    updates

---
+++ src/Conditional_01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Conditional_01 {
+    public static void main(String[] args) {
+        System.out.println("Enter a number:12");
+        Scanner scn = new Scanner(System.in);
+       int num = scn.nextInt();
+
+       if(num > 10){
+           System.out.println("Input is greater than 10");
+       }
+       else{
+           System.out.println("Input is less than 10");
+       }
+    }
+}
