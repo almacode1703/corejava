81b8cfe64e60089b887bebfff01f068d296d40ae
almacode1703 <testmygitt.2023@gmail.com>
2026-02-06 18:36:20 +0530

    updates

---
+++ src/calculator.java
@@ -0,0 +1,18 @@
+public class calculator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 12;
+        int sum = a + b;
+        int diff = a-b;
+        int mul = a*b;
+        float div = (float) a/b;
+        System.out.println("Sum: "+sum);
+        System.out.println("Differnce : "+diff);
+        System.out.println("Multiplication: "+mul);
+        System.out.println("Division : "+div);
+        System.out.println(+(a+b));
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
