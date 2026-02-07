ab756297244d7c8bbea0e497eadfd3bda9e96656
almacode1703 <testmygitt.2023@gmail.com>
2026-02-07 14:55:20 +0530

    updates

---
+++ src/SunnyHot.java
@@ -0,0 +1,18 @@
+public class SunnyHot {
+    public static void main(String[] args) {
+        boolean sunny = true;
+        boolean hot = false;
+        if(sunny){
+            if(hot){
+                System.out.println("Wear a sunglass but no sweater");
+            }
+            else{
+                System.out.println("Wear a Sweater");
+            }
+        }
+        else{
+            System.out.println("Dont wear a Sunglass, but take Raincoat");
+        }
+        System.out.println();
+    }
+}
